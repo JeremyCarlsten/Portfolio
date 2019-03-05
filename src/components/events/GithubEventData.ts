383068f99d852ddcf42d4aa6aca79d87afbc3b45
JeremyCarlsten <jeremy@jeremycarlsten.com>
2019-03-05 19:24:11 +0000

    parsing events from gitlab, need to cache.

--- src/components/events/GithubEventData.ts
+++ src/components/events/GithubEventData.ts
@@ -1,6 +1,4 @@
-const eventHeaderMapping: any = {
-  "CreateEvent": "Created project ",
-  "IssueCommentEvent": "Commented on project ",
+const defaultEventHeaders: any = {
   "ForkEvent": "Forked project ",
   "WatchEvent": "Watched project "
 }
@@ -30,29 +28,65 @@ export default class GithubEventData {
     this.createdAt = new Date(event.created_at);
     switch(event.type){
       case('PushEvent'): {
-        console.log(event.payload)
           this.header = 'Pushed Commits to ' + this.parseRepositoryName(event);
           this.text = `${event.payload.distinct_size} commits on branch ${event.payload.ref.replace(/refs\/heads\//gi, '')}`
         break;
       }
+      case('CreateEvent'): {
+        this.handleCreateEvent(event);
+        break;
+      }
+      case('IssueCommentEvent'): {
+        this.header = `Commented on project ${this.parseRepositoryName(event)}`;
+        this.text = event.payload.comment.body
+        break;
+      }
+      case('PullRequestEvent'): {
+        this.header = `Created Pull Request on project ${this.parseRepositoryName(event)}`;
+        this.text = event.payload.pull_request.body
+        break;
+      }
+      case('ForkEvent'): {
+        this.header = `Forked project ${this.parseRepositoryName(event)}`;
+        this.text = ''
+        break;
+      }
+      case('WatchEvent'): {
+        this.header = `Watched project ${this.parseRepositoryName(event)}`;
+        this.text = ''
+        break;
+      }
       default: {
-        this.header = this.parseHeader(event);
-        this.text = this.parseEventText(event);
+        this.header = 'Oops...'
+        this.text = '';
       }
     }
    
   }
 
+  private handleCreateEvent(event: any) {
+    let type = event.payload.ref_type;
+    if (type == 'branch') {
+      this.header = `Created branch ${event.payload.ref} on ${this.parseRepositoryName(event)}`;
+      this.text = '';
+    }
+    else if (type == 'repository') {
+      this.header = `Created project ${this.parseRepositoryName(event)}`;
+      this.text = event.payload.description;
+    }
+    else {
+      this.header = 'Created ' + this.parseRepositoryName(event);
+      this.text = `asdf`;
+    }
+  }
+
   private parseHeader(event: any): string {
-    return eventHeaderMapping[event.type] + this.parseRepositoryName(event);
+    console.log(event);
+    return defaultEventHeaders[event.type] + this.parseRepositoryName(event);
   }
 
   private parseRepositoryName(event: any): string {
     let regex = new RegExp(`${event.actor.login}/`, "gi")
     return event.repo.name.replace(regex, '')
-  }
-
-  private parseEventText(event: any): string {
-      return 'Did something on github';
   }
 }
