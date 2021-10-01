e8673483b8f2dcb25d2cc1ec4d0ca0dca639c931
Jeremy Carlsten <jcc.993@gmail.com>
2021-10-01 08:17:46 -0500

    polish for the about section

--- src/components/about.js
+++ src/components/about.js
@@ -42,15 +42,17 @@ export default function About() {
           Quickly, I was able to grow in my career, first to the role of a senior developer, and then a team leader while working at Iowa Student Loan.
           Later, I decided it was time to learn something new, and I moved my career into the world of consulting for Lean Techniques.
         </p>
-        <h3>Who I've worked for </h3>
+        <h2>Who I've worked for </h2>
         <Row id="logo-list">
           <Col><a href="http://www.leantechniques.com" target="_blank" rel="noopener noreferrer"><img src={leanTechniques} alt="lean techniques" className="job-logo" /></a></Col>
           <Col><a href="http://www.johndeere.com" target="_blank" rel="noopener noreferrer"><img src={deere} alt="John Deere" className="job-logo" /><br />*Contracted via Lean Techniques</a></Col>
           <Col><a href="http://www.iowastudentloan.org" target="_blank" rel="noopener noreferrer"><img src={iowaStudentLoan} alt="Iowa Student Loan" className="job-logo" /></a></Col>
         </Row>
-        <h3>On a Personal Note</h3>
+        <h2>On a Personal Note</h2>
         <p>I am married to my beautiful wife Kayla who runs a sucessful business of her own, <a href="https://www.calamityroseranch.com">Calamity Rose Ranch</a>. 
-        We have five pretty awesome kids, a small acerage in eastern Iowa with all the animals needed to start a small petting zoo.</p>
+        We have five awesome kids who bring a ton of joy, and chaos, into my life. 
+        And a small acerage in eastern Iowa with all the animals needed to start a small petting zoo.
+        </p>
       </Col>
     </Row>
   );
