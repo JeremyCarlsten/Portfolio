65efec39750c42e9f9ee8e3bf70c6b5c0e101f7a
Jeremy Carlsten <jcc.993@gmail.com>
2021-10-01 08:05:04 -0500

    adding polish to project-item

--- src/components/project-item.js
+++ src/components/project-item.js
@@ -5,8 +5,10 @@ import { Col } from 'react-bootstrap';
 export default function ProjectItem(props) {
     return (
         <Col className="project-item">
-            <img src={props.image} alt={props.imageAlt || ""} className="project-picture" />
-            <h3><a href={props.url} target="_blank" rel="noopener noreferrer">{props.title}</a></h3>
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
+                <img src={props.image} alt={props.imageAlt || ""} className="project-picture" />
+                <h3>{props.title}</h3>
+            </a>
         </Col>
     );
 }
