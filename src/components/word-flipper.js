46be2faabf6ff05d1804658ce3a15f019c126618
Jeremy Carlsten <jcc.993@gmail.com>
2021-10-01 07:52:14 -0500

    adding a word flipper

---
+++ src/components/word-flipper.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/word-flipper.css';
+
+export default function WordFlipper() {
+
+    return (
+        <div className="word-flipper">
+            <span></span>
+            <span>develop</span>
+            <span>make</span>
+            <span>create</span>
+            <span>design</span>
+        </div>
+    );
+}
